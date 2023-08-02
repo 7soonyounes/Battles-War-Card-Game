d802a5273ed8ed2a9eb16d7b49fe27e831e54455
7soonyounes <nonshsoon@gmail.com>
2023-08-02 14:51:23 +0100

    final

--- deck.py
+++ deck.py
@@ -6,7 +6,6 @@ class Deck:
         self.L=[]
 
     def ajoute(self,valeur,couleur):
-        #self.L=t
         c=Carte(valeur,couleur)
         self.L.append(c.image)
 
@@ -14,7 +13,6 @@ class Deck:
         print(len(self.L))
 
     def supprime(self):
-        #a=(len(self.L)-1)
         del(self.L[-1])
 
     def voir(self):
--- main_deck.py
+++
@@ -1,5 +0,0 @@
-from card import Carte
-from deck import Deck
-from main_carte import *
-
-
